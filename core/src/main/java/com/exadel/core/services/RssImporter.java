72eece845d4bcb4f8666ac172ac824476b31ed33
ydobrenchuk <ydobrenchuk@exadel.com>
2021-09-02 12:48:15 +0300

    repo get

--- core/src/main/java/com/exadel/core/services/RssImporter.java
+++ core/src/main/java/com/exadel/core/services/RssImporter.java
@@ -1,5 +1,9 @@
 package com.exadel.core.services;
 
+import com.exadel.core.utility.ManualCard;
+
+import java.util.List;
+
 public interface RssImporter {
-    String getData();
+    List<ManualCard> getData();
 }
--- core/src/main/java/com/exadel/core/services/impl/RssImporterImpl.java
+++ core/src/main/java/com/exadel/core/services/impl/RssImporterImpl.java
@@ -1,41 +1,40 @@
 package com.exadel.core.services.impl;
 
-import com.apptastic.rssreader.Item;
-import com.apptastic.rssreader.RssReader;
+import com.exadel.core.utility.ManualCard;
 import com.exadel.core.services.RssImporter;
-import org.osgi.service.component.annotations.Activate;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component(service = RssImporter.class)
 public class RssImporterImpl implements RssImporter {
 
     private static final Logger LOG = LoggerFactory.getLogger(RssImporterImpl.class);
-    RssReader reader;
-
-    @Activate
-    public void activate() {
-        LOG.info("RSS IMPORTER is ACTIVATED!");
-    }
 
     @Override
-    public String getData() {
-        reader = new RssReader();
-        Stream<Item> rssFeed = null;
-        try {
-            rssFeed = reader.read("http://rss.cnn.com/rss/cnn_topstories.rss");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+    public List<ManualCard> getData() {
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = null;
+        try{
+            URL feedUrl = new URL("https://www.nasa.gov/rss/dyn/breaking_news.rss");
+            feed = input.build(new XmlReader(feedUrl));
+        } catch (IOException | FeedException e) {
+            LOG.error(e.getMessage());
         }
-        List<Item> articles = rssFeed.filter(i -> i.getTitle().equals(Optional.of("space")))
-                                    .collect(Collectors.toList());
-        return articles.toString();
+        List<ManualCard> cards = feed.getEntries().stream().map(x->new ManualCard(x.getTitle(),
+                x.getDescription().getValue(),
+                x.getUri(),
+                x.getPublishedDate())).collect(Collectors.toList());
+
+        return cards;
     }
 }
