b4f3d15dcd0a50164fa6c4e43dd5b314b2471468
ydobrenchuk <ydobrenchuk@exadel.com>
2021-09-07 13:32:21 +0300

    error in model

---
+++ core/src/main/java/com/exadel/core/schedulers/NewsImportScheduler.java
@@ -0,0 +1,59 @@
+package com.exadel.core.schedulers;
+
+import com.exadel.core.utility.ManualCard;
+import com.exadel.core.services.PageService;
+import com.exadel.core.services.RssImporter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.sling.commons.scheduler.Scheduler;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+import java.util.List;
+
+@Slf4j
+@Designate(ocd=NewsImportScheduler.Config.class)
+@Component(service=Runnable.class)
+public class NewsImportScheduler implements Runnable{
+
+    @ObjectClassDefinition(name = "News scheduler",
+            description = "Simple news scheduler")
+    public static @interface Config {
+
+        @AttributeDefinition(name = "Cron-job expression")
+        String scheduler_expression() default "0 0/1 * 1/1 * ? *";
+
+        @AttributeDefinition(name = "Concurrent task",
+                description = "Whether or not to schedule this task concurrently")
+        boolean scheduler_concurrent() default false;
+
+    }
+    @Reference
+    private PageService pageService;
+
+    @Reference
+    private RssImporter rssImporter;
+
+    List<ManualCard> cards;
+    private int schedulerId;
+
+    @Reference
+    private Scheduler scheduler;
+
+    @Activate
+    protected void activate(final Config config){
+    }
+
+    @Override
+    public void run() {
+        log.info("\n===========================NEWS IMPORT SCHEDULER=================================");
+        cards = rssImporter.getData();
+        pageService.createCards(cards);
+    }
+
+
+}
--- core/src/main/java/com/exadel/core/services/impl/PageServiceImpl.java
+++ core/src/main/java/com/exadel/core/services/impl/PageServiceImpl.java
@@ -4,15 +4,19 @@ import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.WCMException;
 import com.exadel.core.services.PageService;
-import com.exadel.core.services.RssImporter;
 import com.exadel.core.utility.ManualCard;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,9 +28,11 @@ public class PageServiceImpl implements PageService {
     @Reference
     private ResourceResolverFactory resolverFactory;
 
+    private String user = "";
     private PageManager pageManager;
     private Session session;
-    private final String CARD_SUPER = "dashboard/components/content/article";
+    private final String PAGE_TAMPLATE = "/apps/dashboard/components/content/article";
+    private final String PAGE_PATH = "/content/dashboard/us/en/news";
 
     @Override
     public Page createCard(ManualCard manualCard) {
@@ -37,8 +43,25 @@ public class PageServiceImpl implements PageService {
             resolver = resolverFactory.getServiceResourceResolver(null);
             session = resolver.adaptTo(Session.class);
             pageManager = resolver.adaptTo(PageManager.class);
-            page = pageManager.create("dashboard/components/content/news", manualCard.getTopic(), CARD_SUPER, "Dynamic Card");
-        } catch (WCMException | LoginException e) {
+            page = pageManager.create(PAGE_PATH, manualCard.getTopic(), PAGE_TAMPLATE, "Dynamic Card");
+            if (page != null) {
+                user = resolver.getUserID();
+
+                Node newNode = page.adaptTo(Node.class);
+                Node cont = newNode.getNode("jcr:content");
+                if (cont != null) {
+                    Node rootNode = session.getRootNode();
+                    String path = rootNode.getPath();
+                    Node parNode = JcrUtils.getNodeIfExists(cont, PAGE_PATH);
+                    Node imageNode = JcrUtils.getOrCreateByPath(parNode.getPath() + "/image", JcrConstants.NT_UNSTRUCTURED, session);
+                    Node textNode = JcrUtils.getNodeIfExists(parNode, "text");
+                    imageNode.setProperty("sling:resourceType", "foundation/components/image");
+                    imageNode.setProperty("fileReference", "/content/dam/we-retail-screens/we-retail-instore-logo.png");
+                    textNode.setProperty("text", "<p>This page is created using page manager</p>");
+                    session.save();
+                }
+            }
+        } catch (WCMException | LoginException | RepositoryException e) {
             log.error("Cannot create card", e);
         }
 
